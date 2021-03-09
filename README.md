9e01633e1f011c93aaa2e83444c5276ee50c743c
Asus <lev.efimov.vel@gmail.com>
2021-03-09 16:17:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+create matrix of field with coordinates ?
+create matrix of coordinates ?
+
+matrix 4x4x4x3 coord + value=(-1, 0, 1)
+
+plot it on the scatter 
+
+_____________________________________
+field debug(f.e. errors with dublicated puns (if 1 on point)) 
+
+script that recognize if vector is direct line
+
+2 player mode
+
+2 player mode through internet
+
+_____________________________________
+
+...
+
+Profit
---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+sns.set(style='darkgrid')
+
+
+class myarray(np.ndarray):
+    def __new__(cls, *args, **kwargs):
+        return np.array(*args, **kwargs).view(myarray)
+
+    def index(self, value):
+        return np.where(self == value)
+
+
+tic_toc_field = np.zeros((4, 4, 4)).astype(int)
+coordinates = np.array(np.meshgrid([0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]))
+coordList = [x for x in np.ndindex(4, 4, 4)]
+coordArray = np.stack([x for x in np.ndindex(4, 4, 4)])
+
+for i in range(3):
+    for ii in range(3):
+        for iii in range(3):
+            coordinates[i, ii, iii]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(coordArray[0], coordArray[1], coordArray[2])
+plt.show()
