66248c1387e7329a9cfd87757bcd04faae8d3dcb
Asus <lev.efimov.vel@gmail.com>
2021-03-09 16:59:28 +0200

    fixed arr of coordinates

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ class myarray(np.ndarray):
 
 
 tic_toc_field = np.zeros((4, 4, 4)).astype(int)
-coordinates = np.array(np.meshgrid([0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]))
-coordList = [x for x in np.ndindex(4, 4, 4)]
+# coordinates = np.array(np.meshgrid([0, 1, 2], [0, 1, 2], [0, 1, 2], [0, 1, 2]))
+# coordList = [x for x in np.ndindex(4, 4, 4)]
 coordArray = np.stack([x for x in np.ndindex(4, 4, 4)])
-
-for i in range(3):
-    for ii in range(3):
-        for iii in range(3):
-            coordinates[i, ii, iii]
+coordArray = coordArray.reshape(192).reshape(4, 4, 4, 3)
+# for i in range(3):
+#     for ii in range(3):
+#         for iii in range(3):
+#             coordinates[i, ii, iii]
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(coordArray[0], coordArray[1], coordArray[2])
+ax.scatter(coordArray[0, :, :], coordArray[1, :, :], coordArray[2, :, :])
 plt.show()
